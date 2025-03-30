d7bd85bd7b307065550c71fea58098c97d633585
seonghyeon19 <qtg7@naver.com>
2025-03-30 23:51:01 +0900

    Update study_screen.dart

--- lib/features/study/study_screen.dart
+++ lib/features/study/study_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StudyScreen extends StatelessWidget {
   const StudyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+          color: Colors.white,
+          child: Padding(
+              padding: EdgeInsets.all(44.w),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 100.h),
+                    Text(
+                      '스터디',
+                      style: TextStyle(
+                        color: const Color(0xFF353C49),
+                        fontSize: 44.sp,
+                        fontFamily: 'Wanted Sans',
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: -0.44.w,
+                      ),
+                    ),
+                  ]
+              )
+          )
+      ),
+    );
   }
 }
+
