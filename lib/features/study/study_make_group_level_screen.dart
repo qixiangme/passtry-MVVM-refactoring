18b59d748b6b66e9e02784ed0e26aa96a24c7b68
seonghyeon19 <qtg7@naver.com>
2025-04-02 02:13:40 +0900

    퀘스트 난이도 선택 스크린 수정 중

--- lib/features/study/study_make_group_level_screen.dart
+++ lib/features/study/study_make_group_level_screen.dart
@@ -30,10 +30,200 @@ class _StudyMakeGroupLevelState extends State<StudyMakeGroupLevel> {
         padding: EdgeInsets.symmetric(horizontal: 77.w, vertical: 20.h),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [SizedBox(height: 150.h)],
-
+          children: [
+            SizedBox(height: 100.h),
+            Text(
+              '퀘스트 난이도를 선택해주세요',
+              style: TextStyle(
+                color: const Color(0xFF1F1F1F),
+                fontSize: 70.sp,
+                fontFamily: 'Wanted Sans',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              '준비 기간이 짧다면 난이도 중 이상을 추천해요',
+              style: TextStyle(
+                color: const Color(0xFF8E95A2),
+                fontSize: 44.sp,
+                fontFamily: 'Wanted Sans',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.44,
+              ),
+            ),
+        Column(
+          children: [
+            Container(
+              width: 485.w,
+              padding: EdgeInsets.only(
+                top: 46.h,
+                left: 70.w,
+                right: 100.w,
+                bottom: 46.h,
+              ),
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    width: 2.75.w,
+                    color: const Color(0xFFEBEBEB),
+                  ),
+                  borderRadius: BorderRadius.circular(38.50.r),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 52.w,
+                children: [
+                  Container(
+                    width: 116.w,
+                    height: 116.w,
+                    decoration: BoxDecoration(color: const Color(0xFFD9D9D9)),
+                  ),
+                  Text(
+                    '난이도 상',
+                    style: TextStyle(
+                      color: const Color(0xFF434343),
+                      fontSize: 40.sp,
+                      fontFamily: 'Wanted Sans',
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 486.w,
+              padding: EdgeInsets.only(
+                top: 46.h,
+                left: 70.w,
+                right: 100.w,
+                bottom: 46.h,
+              ),
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    width: 2.75.w,
+                    color: const Color(0xFFEBEBEB),
+                  ),
+                  borderRadius: BorderRadius.circular(38.50.r),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 52.w,
+                children: [
+                  Container(
+                    width: 116.w,
+                    height: 116.w,
+                    decoration: BoxDecoration(color: const Color(0xFFD9D9D9)),
+                  ),
+                  Text(
+                    '난이도 중',
+                    style: TextStyle(
+                      color: const Color(0xFF434343),
+                      fontSize: 40.sp,
+                      fontFamily: 'Wanted Sans',
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 485.w,
+              padding: EdgeInsets.only(
+                top: 46.h,
+                left: 70.w,
+                right: 100.w,
+                bottom: 46.h,
+              ),
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    width: 2.75.w,
+                    color: const Color(0xFFEBEBEB),
+                  ),
+                  borderRadius: BorderRadius.circular(38.50.r),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 52.w,
+                children: [
+                  Container(
+                    width: 116.w,
+                    height: 116.w,
+                    decoration: BoxDecoration(color: const Color(0xFFD9D9D9)),
+                  ),
+                  Text(
+                    '난이도 하',
+                    style: TextStyle(
+                      color: const Color(0xFF434343),
+                      fontSize: 40.sp,
+                      fontFamily: 'Wanted Sans',
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 486.w,
+              padding: EdgeInsets.only(
+                top: 46.h,
+                left: 70.w,
+                right: 100.w,
+                bottom: 46.h,
+              ),
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    width: 2.75.w,
+                    color: const Color(0xFFEBEBEB),
+                  ),
+                  borderRadius: BorderRadius.circular(38.50.r),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 52.w,
+                children: [
+                  Container(
+                    width: 116.w,
+                    height: 116.w,
+                    decoration: BoxDecoration(color: const Color(0xFFD9D9D9)),
+                  ),
+                  Text(
+                    '난이도 기초',
+                    style: TextStyle(
+                      color: const Color(0xFF434343),
+                      fontSize: 40.sp,
+                      fontFamily: 'Wanted Sans',
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
-    );
+      ]),
+    ));
   }
 }
