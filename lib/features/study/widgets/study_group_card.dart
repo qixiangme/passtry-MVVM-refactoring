3be0db8ec7fb8672e2280a954743bdd450304bb4
ssoosy <soy1006@khu.ac.kr>
2025-04-02 02:09:56 +0900

    study_group_card 테두리, 글씨 부분 크기 등 수정

--- lib/features/study/widgets/study_group_card.dart
+++ lib/features/study/widgets/study_group_card.dart
@@ -12,19 +12,19 @@ class StudyGroupCard extends StatelessWidget {
     return Container(
       width: 482.w,
       height: 434.h,
-      margin: EdgeInsets.symmetric(horizontal: 10.w),
+      margin: EdgeInsets.symmetric(horizontal: 8.w),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(15.r),
+        borderRadius: BorderRadius.circular(35.r),
         border: Border.all(color: Color(0XFFC4CAD4)),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 233.h,
+            height: 255.h,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(15.r)),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(35.r)),
               color: Colors.blueAccent,
               image:
                   group.imageUrl.isNotEmpty
@@ -36,14 +36,14 @@ class StudyGroupCard extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(10.w),
+            padding: EdgeInsets.all(30.w),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   group.name,
                   style: TextStyle(
-                    fontSize: 44.sp,
+                    fontSize: 41.sp,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -52,11 +52,13 @@ class StudyGroupCard extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Icon(Icons.calendar_today, color: Colors.black54),
+                    Icon(Icons.calendar_today, color: Colors.black54, size: 18),
+                    SizedBox(width: 5),
                     Text(group.memberCount),
-                    SizedBox(width: 10),
-                    Icon(Icons.calendar_today, color: Colors.black54),
-                    Text(group.memberCount),
+                    SizedBox(width: 15),
+                    Icon(Icons.calendar_today, color: Colors.black54, size: 18),
+                    SizedBox(width: 5),
+                    Text(group.meetingInfo),
                   ],
                 ),
               ],
